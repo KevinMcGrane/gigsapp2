c730b5e2b95be7d48b6425e7c74788242e0e3854
KevinMcGrane <kevinmcgrane97@gmail.com>
2016-11-07 02:23:24 +0000

    Added age

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ has_many :comments
 has_secure_password
 validates_uniqueness_of :email
 validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+def age
+  now = Time.now.utc.to_date
+  now.year - dob.year - (dob.to_date.change(:year => now.year) > now ? 1 : 0)
+end
 end
